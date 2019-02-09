ca7ab1af7db2861b1ec9c55ac5bfa6ed8511e777
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-02-09 13:18:15 -0500

    primer push

--- app/src/main/java/com/example/asus/dconfo_app/Home_AppActivity.java
+++ app/src/main/java/com/example/asus/dconfo_app/Home_AppActivity.java
@@ -3,11 +3,15 @@ package com.example.asus.dconfo_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+
 public class Home_AppActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__app);
+        BottomBar bottomBar=findViewById(R.id.bar_home_access1);
+        bottomBar.setDefaultTab(R.id.icono_id);
     }
 }
