43252757469ab90cb57a65ed89fb2e0e518d6703
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-02-10 12:48:13 -0500

    primer push

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/administrador/NewListEstudianteFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/administrador/NewListEstudianteFragment.java
@@ -1,15 +1,31 @@
 package com.example.asus.dconfo_app.presentation.view.fragment.administrador;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.asus.dconfo_app.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,15 +35,25 @@ import com.example.asus.dconfo_app.R;
  * Use the {@link NewListEstudianteFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewListEstudianteFragment extends Fragment {
+public class NewListEstudianteFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS =0 ;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button btn_New_L_Estudiantes;
+    private Button btn_Write_New_L_Estudiantes;
+    private ListView lv_lista_est;
+    private TextView txt_mensaje;
+    private Context context;
+    boolean sdDisponible = false;
+    boolean sdAccesoEscritura = false;
+    File ruta_sd;
+    File f;
 
     public static NewListEstudianteFragment getInstance() {
         return new NewListEstudianteFragment();
@@ -70,10 +96,81 @@ public class NewListEstudianteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_new_list_estudiante, container, false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.new_list_estudiante);
+        View view = inflater.inflate(R.layout.fragment_new_list_estudiante, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.new_list_estudiante);
+
+        btn_New_L_Estudiantes = (Button) view.findViewById(R.id.btn_New_listEstudiantes);
+        btn_Write_New_L_Estudiantes = (Button) view.findViewById(R.id.btn_New_Write_listEstudiantes);
+
+        btn_New_L_Estudiantes.setOnClickListener(this);
+        btn_Write_New_L_Estudiantes.setOnClickListener(this);
+
+        lv_lista_est = (ListView) view.findViewById(R.id.lv_NewListEstudiantes);
+        txt_mensaje = (TextView) view.findViewById(R.id.txt_mensaje_list_est);
+        context = getContext();
+        // Código que me comprueba si existe SD y si puedo escribir o no
+        String estado = Environment.getExternalStorageState();
+
+        if (estado.equals(Environment.MEDIA_MOUNTED)) {
+            sdDisponible = true;
+            sdAccesoEscritura = true;
+        } else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            sdDisponible = true;
+            sdAccesoEscritura = false;
+        } else {
+            sdDisponible = false;
+            sdAccesoEscritura = false;
+        }
+
+        //permison en tiempo de ejecución
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            // Show an expanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+
+        } else {
+
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},0
+                    );
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+
         return view;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -98,6 +195,60 @@ public class NewListEstudianteFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.btn_New_listEstudiantes):
+
+                if (sdDisponible) {
+
+                    try {
+                        ruta_sd= Environment.getExternalStorageDirectory();
+
+                        File f = new File(ruta_sd.getAbsolutePath(), "ficherosd.txt");
+
+                        BufferedReader fin =
+                                new BufferedReader(
+                                        new InputStreamReader(
+                                                new FileInputStream(f)));
+
+                        String texto = fin.readLine();
+                        txt_mensaje.setText(texto);
+                        fin.close();
+                    } catch (Exception ex) {
+                        Log.e("Ficheros", "Error al leer fichero desde tarjeta SD");
+                        txt_mensaje.setText("No hay fichero, sdDisponible: "+sdDisponible+" ," +
+                                "sdAccesoEscritura: "+sdAccesoEscritura+" ,RUTA: "+ruta_sd.getAbsolutePath());
+                    }
+
+                }
+                break;
+            case (R.id.btn_New_Write_listEstudiantes):
+                if(sdAccesoEscritura && sdDisponible){
+                    try
+                    {
+                        File ruta_sd = Environment.getExternalStorageDirectory();
+
+                         f= new File(ruta_sd.getAbsolutePath(), "ficherosd.txt");
+
+                        OutputStreamWriter fout =
+                                new OutputStreamWriter(
+                                        new FileOutputStream(f));
+
+                        fout.write("Contenido del fichero de la SD");
+                        fout.close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.e("Ficheros", "Error al escribir fichero en la tarjeta SD");
+                        txt_mensaje.setText("Error al escribir fichero en la tarjeta SD, f :"+f);
+                    }
+                }
+
+                break;
+        }//sw
+    }//onclick
 
     /**
      * This interface must be implemented by activities that contain this
