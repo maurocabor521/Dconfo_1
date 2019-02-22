ca2549894ab9a92f28e5d313e481fecf2ce8692c
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-02-22 08:40:53 -0500

    primer push

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/NewEjercicioDocenteFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/NewEjercicioDocenteFragment.java
@@ -1,4 +1,4 @@
-package com.example.asus.dconfo_app;
+package com.example.asus.dconfo_app.presentation.view.fragment.docente;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.asus.dconfo_app.R;
 
 
 /**
