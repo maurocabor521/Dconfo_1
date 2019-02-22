fba702b61120172be7ed39f52a3ad053c2ea0341
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-02-22 09:26:05 -0500

    primer push

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/NewEjercicioDocenteActivity.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/NewEjercicioDocenteActivity.java
@@ -1,15 +1,60 @@
 package com.example.asus.dconfo_app.presentation.view.activity.docente;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.asus.dconfo_app.R;
+import com.example.asus.dconfo_app.presentation.view.fragment.docente.HomeEjerciciosDocenteFragment;
+import com.example.asus.dconfo_app.presentation.view.fragment.docente.NewEjercicioDocenteFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class NewEjercicioDocenteActivity extends AppCompatActivity {
-
+    private BottomBar bottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_ejercicio_docente);
+        showToolbar("Nuevo Ejercicio Léxico", true);
+        bottomBar = findViewById(R.id.bottombar_CED_activity);
+    }
+
+    private void cargarBottombar() {
+
+        bottomBar.setDefaultTab(R.id.bott_home_CED);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId) {
+                    case R.id.bott_home_CED:
+                        homeEjerciciosDocenteFragment = new HomeEjerciciosDocenteFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_DocenteEjercicios, homeEjerciciosDocenteFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        //Toast.makeText(getApplicationContext(), "Ejercicios Home", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.bott_new_CED:
+                        newEjercicioDocenteFragment = new NewEjercicioDocenteFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_DocenteEjercicios, newEjercicioDocenteFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        //Toast.makeText(getApplicationContext(), "Ejercicio Nuevo", Toast.LENGTH_LONG).show();
+                        break;
+
+
+                }
+            }
+        });
+    }
+
+
+    public void showToolbar(String tittle, boolean upButton) {
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_ejercicio);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        //getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
