63516f673991a4797ca4666f6c2f926ae8e23f69
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-02-22 10:56:45 -0500

    primer push

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/tipoFragments/HomeTiposFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/tipoFragments/HomeTiposFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -70,7 +71,10 @@ public class HomeTiposFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_tipos, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_home_tipos, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.home_tipo);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/tipoFragments/Tipo1Fragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/tipoFragments/Tipo1Fragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,7 +70,10 @@ public class Tipo1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tipo1, container, false);
+        View view=inflater.inflate(R.layout.fragment_tipo1, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.home_tipo1);
+        return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
