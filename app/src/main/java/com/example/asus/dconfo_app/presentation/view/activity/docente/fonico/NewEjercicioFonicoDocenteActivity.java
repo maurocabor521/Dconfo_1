0514d8e2f0c4b4b83e117a8eeca4778cb00abdb5
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-04-27 12:00:18 -0500

    commit new pc

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/fonico/NewEjercicioFonicoDocenteActivity.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/fonico/NewEjercicioFonicoDocenteActivity.java
@@ -5,8 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.asus.dconfo_app.R;
+import com.example.asus.dconfo_app.presentation.view.fragment.docente.fonico.HomeFonicoFragment;
+import com.example.asus.dconfo_app.presentation.view.fragment.docente.fonico.Tipo1FonicoFragment;
+import com.example.asus.dconfo_app.presentation.view.fragment.docente.fonico.Tipo2FonicoFragment;
 import com.example.asus.dconfo_app.presentation.view.fragment.docente.tipoFragments.HomeTiposFragment;
 import com.example.asus.dconfo_app.presentation.view.fragment.docente.tipoFragments.Tipo1Fragment;
 import com.example.asus.dconfo_app.presentation.view.fragment.docente.tipoFragments.Tipo2Fragment;
@@ -14,15 +18,17 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity implements
-        HomeTiposFragment.OnFragmentInteractionListener,
-        Tipo1Fragment.OnFragmentInteractionListener,
-        Tipo2Fragment.OnFragmentInteractionListener {
+        HomeFonicoFragment.OnFragmentInteractionListener,
+        Tipo1FonicoFragment.OnFragmentInteractionListener,
+        Tipo2FonicoFragment.OnFragmentInteractionListener {
+
     private BottomBar bottomBar;
-    private HomeTiposFragment homeTiposFragment;
-    private Tipo1Fragment tipo1Fragment;
-    private Tipo2Fragment tipo2Fragment;
-    String nameDocente="";
-    int idDocente=0;
+    private HomeFonicoFragment homeFonicoFragment;
+    private Tipo1FonicoFragment tipo1FonicoFragment;
+    private Tipo2FonicoFragment tipo2FonicoFragment;
+
+    String nameDocente = "";
+    int idDocente = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +38,11 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
         Bundle extra = intent.getExtras();
 
         nameDocente = extra.getString("namedocente");
-        idDocente= extra.getInt("iddocente");
+        idDocente = extra.getInt("iddocente");
 
-        showToolbar("Nuevo Ejercicio Fónico, Docente: "+nameDocente, true);
+        showToolbar("Nuevo Ejercicio Fónico, Docente: " + nameDocente, true);
         bottomBar = findViewById(R.id.bottombar_CED_activity);
-       // cargarBottombar();
+        cargarBottombar();
     }
 
     //método que permite volver al padre conservando las variables
@@ -46,7 +52,7 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
         return true;
     }
 
- /*   private void cargarBottombar() {
+    private void cargarBottombar() {
 
         bottomBar.setDefaultTab(R.id.bott_home_tipo_CED);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
@@ -60,13 +66,13 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
                         args1.putString("namedocente", nameDocente);
                         args1.putInt("iddocente", idDocente);
 
-                        homeTiposFragment = new HomeTiposFragment();
-                        homeTiposFragment.setArguments(args1);
+                        homeFonicoFragment = new HomeFonicoFragment();
+                        homeFonicoFragment.setArguments(args1);
 
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CED, homeTiposFragment)
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CEFD, homeFonicoFragment)
                                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                                 .addToBackStack(null).commit();
-                        //Toast.makeText(getApplicationContext(), "Ejercicios Home", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Ejercicios Home", Toast.LENGTH_LONG).show();
                         break;
                     case R.id.bott_tipo1_CED:
                         //enviar datos activity a fragment
@@ -76,13 +82,13 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
                         args.putString("namedocente", nameDocente);
                         args.putInt("iddocente", idDocente);
 
-                        tipo1Fragment = new Tipo1Fragment();
-                        tipo1Fragment.setArguments(args);
+                        tipo1FonicoFragment = new Tipo1FonicoFragment();
+                        tipo1FonicoFragment.setArguments(args);
 
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CED, tipo1Fragment)
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CEFD, tipo1FonicoFragment)
                                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                                 .addToBackStack(null).commit();
-                        //Toast.makeText(getApplicationContext(), "Ejercicio Nuevo", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Ejercicio tipo 1", Toast.LENGTH_LONG).show();
                         break;
                     case R.id.bott_tipo2_CED:
 
@@ -90,10 +96,10 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
                         args2.putString("namedocente", nameDocente);
                         args2.putInt("iddocente", idDocente);
 
-                        tipo2Fragment = new Tipo2Fragment();
-                        tipo2Fragment.setArguments(args2);
+                        tipo2FonicoFragment = new Tipo2FonicoFragment();
+                        tipo2FonicoFragment.setArguments(args2);
 
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CED, tipo2Fragment)
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_Tipos_CEFD, tipo2FonicoFragment)
                                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                                 .addToBackStack(null).commit();
                         //Toast.makeText(getApplicationContext(), "Ejercicio Nuevo", Toast.LENGTH_LONG).show();
@@ -105,7 +111,7 @@ public class NewEjercicioFonicoDocenteActivity extends AppCompatActivity impleme
         });
     }
 
-*/
+
     public void showToolbar(String tittle, boolean upButton) {
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_ejercicio);
         setSupportActionBar(toolbar);
