84ea91c84952ed8f286213f5b798503094c41f6a
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-04-30 16:56:39 -0500

    commit new pc

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/fonico/Tipo1FonicoFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/fonico/Tipo1FonicoFragment.java
@@ -1,14 +1,34 @@
 package com.example.asus.dconfo_app.presentation.view.fragment.docente.fonico;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.asus.dconfo_app.R;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +47,38 @@ public class Tipo1FonicoFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private EditText edt_letra;
+    private Button btn_img1;
+    private Button btn_img2;
+    private Button btn_img3;
+    private Button btn_img4;
+    private Button btn_enviar;
+
+    private boolean btn1Activo = false;
+    private boolean btn2Activo = false;
+    private boolean btn3Activo = false;
+    private boolean btn4Activo = false;
+
+    ProgressDialog progreso;
+    ImageView imgFoto;
+    File fileImagen;
+    Bitmap bitmap;
+
+    private final int MIS_PERMISOS = 100;
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
+
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp/";//directorio principal
+    private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
+    private String path;//almacena la ruta de la imagen
+
+    //******** CONEXIÓN CON WEBSERVICE
+    //RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+
+    StringRequest stringRequest;
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,8 +117,156 @@ public class Tipo1FonicoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tipo1_fonico, container, false);
+        View view = inflater.inflate(R.layout.fragment_tipo1_fonico, container, false);
+        edt_letra = (EditText) view.findViewById(R.id.edt_fonico_tipo1_vocal);
+        btn_enviar = (Button) view.findViewById(R.id.btn_fonico_doc_enviar);
+        btn_img1 = (Button) view.findViewById(R.id.btn_fonico_doc_img1);
+        btn_img2 = (Button) view.findViewById(R.id.btn_fonico_doc_img2);
+        btn_img3 = (Button) view.findViewById(R.id.btn_fonico_doc_img3);
+        btn_img4 = (Button) view.findViewById(R.id.btn_fonico_doc_img4);
+
+        btn_img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1Activo = true;
+                mostrarDialogOpciones();
+            }
+        });
+        btn_img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn2Activo = true;
+                mostrarDialogOpciones();
+            }
+        });
+        btn_img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn3Activo = true;
+                mostrarDialogOpciones();
+            }
+        });
+        btn_img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn4Activo = true;
+                mostrarDialogOpciones();
+            }
+        });
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        imgFoto = new ImageView(getContext());
+        return view;
     }
+    //**********************************************************************************************
+
+    private void mostrarDialogOpciones() {//part 9
+        final CharSequence[] opciones = {"Tomar Foto", "Elegir de Galeria", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Elige una Opción");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")) {
+                    //abriCamara();//part 10 tomar foto
+                    Toast.makeText(getContext(), "Cargar Cámara", Toast.LENGTH_LONG).show();
+                } else {
+                    if (opciones[i].equals("Elegir de Galeria")) {
+                        /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);*/
+                        //directamente de galeria
+                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent, "Seleccione"), COD_SELECCIONA);
+                    } else {
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {//p9 de startActivityForResult
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case COD_SELECCIONA:
+                Uri miPath = data.getData();
+                imgFoto.setImageURI(miPath);
+                try {//p12
+                    bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), miPath);
+                    imgFoto.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                cargarImagen();
+                break;
+
+            case COD_FOTO://p10
+                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("Path", "" + path);
+                            }
+                        });
+
+                bitmap = BitmapFactory.decodeFile(path);
+                imgFoto.setImageBitmap(bitmap);
+
+                break;
+        }
+        bitmap = redimensionarImagen(bitmap, 600, 800);//part 14 redimencionar imágen,guarde en carpeta y BD
+    }
+
+    private void cargarImagen() {
+        Drawable drawable = imgFoto.getDrawable();
+        if (btn1Activo) {
+            btn_img1.setBackground(drawable);
+            btn1Activo = false;
+        } else if (btn2Activo) {
+            btn_img2.setBackground(drawable);
+            btn2Activo = false;
+        } else if (btn3Activo) {
+            btn_img3.setBackground(drawable);
+            btn3Activo = false;
+        } else if (btn4Activo) {
+            btn_img4.setBackground(drawable);
+            btn4Activo = false;
+        }
+        // btn_Tipo1_pic_Ejercicio.setBackground(drawable);
+        //imageView_muestra.setBackground(drawable);
+    }
+
+
+    private Bitmap redimensionarImagen(Bitmap bitmap, float anchoNuevo, float altoNuevo) {//part 14
+
+        int ancho = bitmap.getWidth();
+        int alto = bitmap.getHeight();
+
+        if (ancho > anchoNuevo || alto > altoNuevo) {
+            float escalaAncho = anchoNuevo / ancho;
+            float escalaAlto = altoNuevo / alto;
+
+            Matrix matrix = new Matrix();//manipular datos internos de la imagen
+            matrix.postScale(escalaAncho, escalaAlto);
+
+            return Bitmap.createBitmap(bitmap, 0, 0, ancho, alto, matrix, false);
+
+        } else {
+            return bitmap;
+        }
+    }
+
+
+    //**********************************************************************************************
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
