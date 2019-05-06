fe1cc060cc62f94d9d307dd20eb6aca6d83cbb6b
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-05-06 16:59:19 -0500

    commit new pc

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/Estudiante/fonico/Tipo1FonicoFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/Estudiante/fonico/Tipo1FonicoFragment.java
@@ -65,7 +65,7 @@ public class Tipo1FonicoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_tipo1_fonico2, container, false);
+        View view=inflater.inflate(R.layout.fragment_tipo1_fonico_Estudiante, container, false);
 
         return view;
     }
