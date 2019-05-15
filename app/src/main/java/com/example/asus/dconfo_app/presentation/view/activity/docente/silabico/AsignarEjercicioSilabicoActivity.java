cd84f5bc8d75cb2ccd159d81399c10e20a6d2d31
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-05-15 14:01:23 -0500

    commit new pc

---
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/silabico/AsignarEjercicioSilabicoActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.dconfo_app.presentation.view.activity.docente.silabico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.dconfo_app.R;
+
+public class AsignarEjercicioSilabicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asignar_ejercicio_silabico);
+    }
+}
